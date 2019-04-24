d25766ece13a7a7f4879a6b2610a722d0e666f03
Raul Rosá <raul.hmetal@gmail.com>
2019-04-24 10:05:23 -0300

    Get Schedules updated

--- ProjetoMinions/js/admin.js
+++ ProjetoMinions/js/admin.js
@@ -12,11 +12,11 @@
 
 /********************
 
-	GET SCHEDULES
+	SCHEDULES
 
 *******************/
 
-window.addEventListener('load', async function(){
+const getSchedules = async function(){
 	await axios.get('/schedules')
 	.then(res => {
 		if(!res.data.error){
@@ -64,22 +64,56 @@ window.addEventListener('load', async function(){
 				}
 			});
 
-			let today = new Date();
-			let tableHeader = ['Horários', 'Hoje'];
-			for(let i = 1; i < 7; i++)
-				tableHeader.push(getDateInStandard(today, i));
+			let tableHeader = getScheduleHeader();
+			let columnHeader = getHourIntervals(startTime, endTime);
 
-			console.log(schedules);
-			console.log(weekDays);
-			console.log(tableHeader);
-
-			//let scheduleMatrix = [];
-			//for(let i = startTime; i < endTime; i++){
-
-			//}
+			fillScheduleTable(weekDays, tableHeader, columnHeader);
 		}
 	});
-});
+}
+
+const fillScheduleTable = function(content, header, columnHeader){
+	let scheduleTable = gId("schedule-table");
+
+	//Add the table header
+	let tHead = document.createElement('thead');
+		let tHeadRow = document.createElement('tr');
+			for(let i = 0; i < header.length; i++)
+				tHeadRow.appendChild(addTh(header[i].date, 'col'));
+		tHead.appendChild(tHeadRow);
+	scheduleTable.appendChild(tHead);
+
+	console.log(content);
+
+	let tBody = document.createElement('tbody');
+		columnHeader.forEach((hd) => {
+			let tBodyRow = document.createElement('tr');
+				tBodyRow.appendChild(addTh(hd.interval, 'row'));
+
+				for(let i = 0; i < content.length; i++){
+					let obj = content[i].find((obj) => {return obj.time == hd.startTime});
+					let date = new Date();
+					let tdClass = "";
+					let notifications = null;
+					if(date.getHours() >= hd.startTime && date.getDay() == header[i+1].day)
+						tdClass = "bg-secondary"; //finished
+					else if(obj === undefined)
+						tdClass = "bg-danger"; //not exists
+					else if(obj.open == 0)
+						tdClass = "bg-danger"; //closed
+					//Add the reserved color and notifications.
+					tBodyRow.appendChild(addTd(tdClass, notifications));
+				}
+			tBody.appendChild(tBodyRow);
+		});
+	scheduleTable.appendChild(tBody);
+}
+
+/*******************
+
+	UTILITIES
+
+*******************/
 
 Array.prototype.move = function (from, to) {
   this.splice(to, 0, this.splice(from, 1)[0]);
@@ -91,5 +125,51 @@ const getDateInStandard = function(date, dayOffset){
 	let dd = String(newDay.getDate()).padStart(2, '0');
 	let mm = String(newDay.getMonth()+1).padStart(2, '0');
 
-	return dd + '/' + mm;
+	return {date: dd + '/' + mm, day: dd };
 }
+
+const getScheduleHeader = function(){
+	let today = new Date();
+	let tableHeader = [{date: 'Horários', day: null }, {date: 'Hoje', day: today.getDay() }];
+	for(let i = 1; i < 7; i++)
+		tableHeader.push(getDateInStandard(today, i));
+	return tableHeader;
+}
+
+const getHourIntervals = function(startTime, endTime){
+	let intervals = [];
+	for(let i = startTime; i < endTime; i++){
+		intervals.push({interval:  `${i}:00 - ${i+1}:00`, startTime: i});
+	}
+	return intervals;
+}
+
+const addTh = function(text, scope){
+	let th = document.createElement('th');
+	th.scope = scope;
+	th.appendChild(document.createTextNode(text));
+	return th;
+}
+
+const addTd = function(tdClass, badge){
+	let td = document.createElement('td');
+	td.className += tdClass;
+	if(badge){
+		let span = document.createElement('span');
+		span.className += "badge badge-light";
+		span.appendChild(document.createTextNode(badge));
+		td.appendChild(span);
+	}
+
+	return td;
+}
+
+/******************
+
+	EVENTS
+
+*******************/
+
+window.addEventListener('load', ()=> {
+	getSchedules();
+});
